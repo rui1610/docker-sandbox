2977dff275a396c30cc9ee8ea87ae8a9e2c4ed26
rui1610 <rui.nogueira1610@hotmail.com>
2022-03-20 09:36:28 +0100

    fix major bugs

--- multimedia/mp3metadatafetcher/watchMediaFolder.py
+++ multimedia/mp3metadatafetcher/watchMediaFolder.py
@@ -1,17 +1,26 @@
 import inotify.adapters
 import os
 from mp3metadatafetcher import addMetadataToFiles
-i = inotify.adapters.InotifyTree('media')
+from pathlib import Path 
+
+WATCHFOLDER = "media"
+
+def splitFileName(file):
+    dir = os.path.dirname(os.path.realpath(file))
+    name = Path(file).name
+    prefix, suffix = name.split(".")
+    return dir,name,prefix,suffix
+
+i = inotify.adapters.InotifyTree(WATCHFOLDER)
 
 for event in i.event_gen(yield_nones=False):
   (_, type_names, path, filename) = event
 
   for event_type in type_names:
-    print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format( path, filename, type_names))    
-    filenameOnly = os.path.basename(filename)
-    filenameBase = os.path.splitext(filenameOnly)[0]
-    print (filenameOnly + " - " + filenameBase )
-    if event_type == "IN_CREATE": 
-      if (filenameBase == "mp3"):
+    #print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format( path, filename, type_names))    
+    dir,name,prefix,suffix = splitFileName(filename)
+    if event_type == "IN_CLOSE_WRITE": 
+      if (suffix == "mp3"):
+        print("detected new file " + name + " in folder " + WATCHFOLDER)
         addMetadataToFiles()
 

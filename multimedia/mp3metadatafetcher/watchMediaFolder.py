1d8bc42334ce6813b936c8c19caec201f4bda6b5
rui1610 <rui.nogueira1610@hotmail.com>
2022-03-20 08:51:47 +0100

    add log info

--- multimedia/mp3metadatafetcher/watchMediaFolder.py
+++ multimedia/mp3metadatafetcher/watchMediaFolder.py
@@ -7,9 +7,11 @@ for event in i.event_gen(yield_nones=False):
   (_, type_names, path, filename) = event
 
   for event_type in type_names:
+    print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format( path, filename, type_names))    
+    filenameOnly = os.path.basename(filename)
+    filenameBase = os.path.splitext(filenameOnly)[0]
+    print (filenameOnly + " - " + filenameBase )
     if event_type == "IN_CREATE": 
-      filenameOnly = os.path.basename(filename)
-      filenameBase = os.path.splitext(filenameOnly)[0]
       if (filenameBase == "mp3"):
         addMetadataToFiles()
 

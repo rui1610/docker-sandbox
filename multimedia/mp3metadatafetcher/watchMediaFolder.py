ac56fd31d694e1ae825dd5db2845394b1ee5b875
rui1610 <rui.nogueira1610@hotmail.com>
2022-03-20 09:51:10 +0100

    improve detection of mp3 files

--- multimedia/mp3metadatafetcher/watchMediaFolder.py
+++ multimedia/mp3metadatafetcher/watchMediaFolder.py
@@ -5,12 +5,6 @@ from pathlib import Path
 
 WATCHFOLDER = "media"
 
-def splitFileName(file):
-    dir = os.path.dirname(os.path.realpath(file))
-    name = Path(file).name
-    prefix, suffix = name.split(".")
-    return dir,name,prefix,suffix
-
 i = inotify.adapters.InotifyTree(WATCHFOLDER)
 
 for event in i.event_gen(yield_nones=False):
@@ -18,9 +12,8 @@ for event in i.event_gen(yield_nones=False):
 
   for event_type in type_names:
     #print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format( path, filename, type_names))    
-    dir,name,prefix,suffix = splitFileName(filename)
     if event_type == "IN_CLOSE_WRITE": 
-      if (suffix == "mp3"):
-        print("detected new file " + name + " in folder " + WATCHFOLDER)
+      if filename.endswith('.mp3'):
+        print("detected new file " + filename + " in folder " + WATCHFOLDER)
         addMetadataToFiles()
 

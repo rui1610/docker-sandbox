b2495d1f3af16ffa5d5dd8ea0c66e3115abd6889
rui1610 <rui.nogueira1610@hotmail.com>
2022-04-12 20:13:29 +0200

    add capability to switch artist & title for search

--- multimedia/mp3tool/helperItunes.py
+++ multimedia/mp3tool/helperItunes.py
@@ -1,10 +1,12 @@
+from distutils.util import split_quoted
 import time
 import requests
 import urllib.parse
 
 from helperGeneric import checkIfGoodResult
 
-def getMetadataFromItunes(searchString):
+
+def sendRequestToItunes(searchString):
 
     maximumResults = 50
     searchString = searchString.replace(':', '/')
@@ -12,8 +14,6 @@ def getMetadataFromItunes(searchString):
     term = {"term":searchString}
 
     termUrlEncoded = urllib.parse.urlencode(term)
-
-
     req_string = 'https://itunes.apple.com/search?' + termUrlEncoded + '&entity=musicTrack&limit=' + str(maximumResults)
 
     try:
@@ -21,16 +21,11 @@ def getMetadataFromItunes(searchString):
         # Checkout https://developer.apple.com/forums/thread/66399?page=2 for some more information
         time.sleep(3)
         response = requests.get(req_string)
-        result = []
         if response.status_code == 200:
             response = response.json()
-            for thisResponse in response['results']:
-                artist = thisResponse['artistName']
-                title = thisResponse['trackName']
-                goodResult = checkIfGoodResult(searchString, artist, title)
-                if (goodResult == True and len(result) < 5):
-                    result.append(thisResponse)
-        return result
+            return response
+        else:
+            return None
 
     except Exception as e:
         print (" - addItunesCoverArt: EXCEPTION found")
@@ -38,6 +33,24 @@ def getMetadataFromItunes(searchString):
         print ("   >> " + str(e))
         return None
 
-    return None
 
+def getMetadataFromItunes(searchString):
 
+    response = sendRequestToItunes(searchString)
+    # If nothing is found try to switch artist and title
+    if response == None:
+        splitChars = " - "
+        splitText = searchString.split(splitChars)
+        if len(splitText) == 2:
+            searchString = splitText[1] + splitChars + splitText[0]
+            response = sendRequestToItunes(searchString)
+
+    result = []
+    for thisResponse in response['results']:
+        artist = thisResponse['artistName']
+        title = thisResponse['trackName']
+        goodResult = checkIfGoodResult(searchString, artist, title)
+        if (goodResult == True and len(result) < 5):
+            result.append(thisResponse)
+
+    return result

28046bcb5e1fc9f675f4d222e687c22aec86347b
rui1610 <rui.nogueira1610@hotmail.com>
2022-06-12 17:45:03 +0200

    updates

--- raspi-emulator/run.sh
+++ raspi-emulator/run.sh
@@ -3,3 +3,8 @@
 docker build -t pidoc .
 docker run -itd --name testnode pidoc
 docker logs testnode -f
+docker kill testnode
+docker container rm testnode
+docker run -itd -p 127.0.0.1:2222:2222 --name testnode pidoc
+docker logs testnode -f
+ssh user@password -p 2222
